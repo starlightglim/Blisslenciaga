7dc3707118150476f455d2e33b5b164f36d39a9a
starlightglim <195524079+starlightglim@users.noreply.github.com>
2025-01-19 19:48:52 -0500

    placeholder frontpage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { motion, AnimatePresence } from 'framer-motion'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div key={router.route}>
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  )
 }
